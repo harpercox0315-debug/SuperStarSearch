f81c83830fb4ae17dc2af74be78d2cb9b175ec5d
harpercox0315-debug <harper.cox0315@gmail.com>
2026-02-03 16:49:31 -0500

    app.js

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ setTimeout(() => {
   document.getElementById("app").hidden = false;
   loadHub();
 }, 2500);
+function bootOS() {
+  const app = document.getElementById("app");
+
+  app.innerHTML = `
+    <div class="boot-screen">
+      <h1>SUPERSTARSEARCH</h1>
+      <p>Launching OS...</p>
+    </div>
+  `;
+
+  setTimeout(() => {
+    loadHub();
+  }, 1500);
+}
+
+bootOS();
 
