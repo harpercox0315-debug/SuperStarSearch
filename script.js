b716ba66023f5e44c7ad3b92522e8338c855389b
harpercox0315-debug <harper.cox0315@gmail.com>
2026-02-03 16:31:09 -0500

    script.js

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ setTimeout(() => {
   document.getElementById("boot-screen").style.display = "none";
   document.getElementById("os").hidden = false;
 }, 2000);
+setTimeout(() => {
+  document.getElementById("boot-screen").style.display = "none";
+  document.getElementById("app").hidden = false;
+  loadHub();
+}, 2500);
 
